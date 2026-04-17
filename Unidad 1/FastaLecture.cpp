dfdc8fa73d843dec83d001b85885fb22c812b707
2023JoshuaP <jphilco@unsa.edu.pe>
2026-04-17 07:48:25 -0500

    Adding struct metrics (maximum, minimum and average) and proportion GC.

--- Unidad 1/FastaLecture.cpp
+++ Unidad 1/FastaLecture.cpp
@@ -2,10 +2,19 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <iomanip>
+
+#define ll long long
 
 struct FastaRecordStructure {
     std::string identifier;
     std::string sequence;
+};
+
+struct FastaMetrics {
+    ll maximum;
+    ll minimum;
+    double average;
 };
 
 void read_file_fasta(const std::string &file_fasta, std::vector<FastaRecordStructure> &records) {
@@ -40,6 +49,42 @@ void read_file_fasta(const std::string &file_fasta, std::vector<FastaRecordStruc
     file.close();
 }
 
+FastaMetrics calculate_metrics(const std::vector<FastaRecordStructure>& records) {
+    FastaMetrics metrics_fasta = {0, LLONG_MAX, 0.0};
+    ll total_length = 0;
+
+    for (const auto &record : records) {
+        ll sequence_length = record.sequence.length();
+        total_length += sequence_length;
+
+        if (sequence_length > metrics_fasta.maximum) {
+            metrics_fasta.maximum = sequence_length;
+        }
+
+        if (sequence_length < metrics_fasta.minimum) {
+            metrics_fasta.minimum = sequence_length;
+        }
+    }
+
+    metrics_fasta.average = records.empty() ? 0.0 : static_cast<double>(total_length) / records.size();
+
+    return metrics_fasta;
+}
+
+double count_proportion_gc(const std::string &sequence) {
+    ll gc_count = 0;
+
+    for (char gc : sequence) {
+        if (gc == 'G' || gc == 'C' || gc == 'g' || gc == 'c') {
+            gc_count++;
+        }
+    }
+
+    double proportion_gc = sequence.empty() ? 0.0 : (static_cast<double>(gc_count) / sequence.length()) * 100.0;
+
+    return proportion_gc;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <file.fasta>" << std::endl;
@@ -50,6 +95,8 @@ int main(int argc, char *argv[]) {
     std::string file_fasta = argv[1];
     read_file_fasta(file_fasta, records);
 
+    FastaMetrics metrics = calculate_metrics(records);
+
     const int LIMIT = 100;
 
     for (const auto &record : records) {
@@ -59,6 +106,17 @@ int main(int argc, char *argv[]) {
         } else {
             std::cout << record.sequence << "\n";
         }
+    }
+
+    std::cout << "------------------------------\n";
+    std::cout << "Maximum sequence length: " << metrics.maximum << "\n";
+    std::cout << "Minimum sequence length: " << metrics.minimum << "\n";
+    std::cout << "Average sequence length: " << metrics.average << "\n";
+    std::cout << "------------------------------\n";
+    std::cout << "Proportion GC per sequence:\n";
+
+    for (const auto &record : records) {
+        std::cout << record.identifier << ": " << std::fixed << std::setprecision(2) << count_proportion_gc(record.sequence) << "%\n";
     }
 
     return 0;
