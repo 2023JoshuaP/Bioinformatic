b28d97119533cc062d5deb23eea22eb4ea408eeb
2023JoshuaP <jphilco@unsa.edu.pe>
2026-07-07 12:26:19 -0500

    Phase initial to De Bruijn algorithm

--- Unidad 3/SequenceAssembly.cpp
+++ Unidad 3/SequenceAssembly.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 using KmerMap = unordered_map<string, int>;
+using Graph = unordered_map<string, vector<string>>;
 
 KmerMap countMers(const string &sequence, int k) {
     KmerMap frequence;
@@ -24,6 +25,54 @@ KmerMap countMers(const string &sequence, int k) {
     return frequence;
 }
 
+KmerMap combineMers(const vector<string> &fragments, int k) {
+    KmerMap global;
+
+    for (const auto &fragment : fragments) {
+        KmerMap local = countMers(fragment, k);
+        for (const auto &[kmer, count] : local) {
+            global[kmer] += count;
+        }
+    }
+
+    return global;
+}
+
+Graph BuildDeBruijnGraph(const KmerMap &kmerCounts) {
+    Graph graph;
+
+    for (const auto &[kmer, count] : kmerCounts) {
+        string prefix = kmer.substr(0, kmer.size() - 1);
+        string suffix = kmer.substr(1);
+        for (int i = 0; i < count; ++i) {
+            graph[prefix].push_back(suffix);
+        }
+    }
+
+    return graph;
+}
+
+void printGraph(const Graph &graph) {
+    for (const auto &[node, edges] : graph) {
+        cout << node << " -> ";
+        for (const auto &edge : edges) {
+            cout << edge << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main() {
+    vector<string> fragments = { "ATGCGATGAC" };
+    int k = 4;
+    KmerMap kmers = combineMers(fragments, k);
+    cout << "Generate Kmers:" << endl;
+    for (const auto &[kmer, count] : kmers) {
+        cout << kmer << ": " << count << endl;
+    }
+
+    cout << "\nBuild De Bruijn Graph:" << endl;
+    Graph graph = BuildDeBruijnGraph(kmers);
+    printGraph(graph);
     return 0;
 }
